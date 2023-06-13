4e795f9f684a6b07541dfcafc61cb13c368c9338
Meet3456 <meetvasa09@gmail.com>
2023-06-13 12:26:26 +0530

    pkl file added

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('./notebooks/movies.pkl','rb'))
-similarity = pickle.load(open('./notebooks/similarity.pkl','rb'))
+movies = pickle.load(open('/notebooks/movies.pkl','rb'))
+similarity = pickle.load(open('/notebooks/similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
